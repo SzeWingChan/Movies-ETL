4c7c99b61579560d06d64ce92eff08d44e641465
Tracy Chan <100324733+SzeWingChan@users.noreply.github.com>
2022-04-23 19:39:06 -0400

    Add files via upload
    Completed 8.5.1

---
+++ 8.5.1_Schema.sql
@@ -0,0 +1,8 @@
+--Check if the movies_df is loaded to a SQL table 
+--1. Right-click the "movies" table name and select Properties.
+--2. Click the Columns tab to make sure all columns have an appropriate data type.
+--3. Close the Properties window, and then right-click "movies" again.
+--4. Select "View/Edit Data" followed by "First 100 Rows."
+--5. Right-click "movies" and select Query Tool.
+SELECT COUNT(*) FROM movies
+
